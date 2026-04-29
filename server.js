24c0ba0ca25e1d06aa0b0c76cd6c8112b141bb17
NiVi-2005 <rnivetha2005@gmail.com>
2026-04-29 19:21:37 +0530

    fix cors for vercel deployed frontend

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ const app = express()
 const allowedOrigins = [
   process.env.FRONTEND_URI,
   "http://localhost:5173",
-  "http://localhost:3000"
+  "http://localhost:3000",
+  "https://medi-match-omega.vercel.app"
 ];
 
 
